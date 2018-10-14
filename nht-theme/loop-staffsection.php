b03741fb58dbb4d0f2c87e7b0dbf2b9db6d866d3
Henry Holtgeerts <henryholtgeerts@gmail.com>
2018-10-14 13:14:33 -0500

    staff section loop istead of objects

--- nht-theme/loop-staffsection.php
+++ nht-theme/loop-staffsection.php
@@ -6,58 +6,64 @@ if( have_rows('staff_sections') ):
  	// loop through the rows of data
     while ( have_rows('staff_sections') ) : the_row(); ?>
 
-        <?php
-		
-        // vars
-        $section = the_sub_field('staff_section');	
-        
-        if( $section ): ?>
+        <?php if( have_rows('staff_section') ): 
 
-		<div class="nht-row">
-        	<h3 class='nht-heading__text'>
-                <?php echo $section['section_title']; ?>
-			</h3>
-        </div>
+            while( have_rows('staff_section') ): the_row(); 
+    
+            // vars
+            $section_title = get_sub_field('section_title');
+            $post_objects = get_sub_field('section_staff');
+            
+            ?>
 
-        <?php
+            <div class="nht-row">
+                <h3 class='nht-heading__text'>
+                    <?php echo $section_title; ?>
+                </h3>
+            </div>
 
-        $post_objects = $section['section_staff'];
+            <?php
 
-        if( $post_objects ): ?>
-                <?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
-                    <?php setup_postdata($post); ?>
-                <div class="nht-card nht-card--story">
-                    <div class="nht-card__inner">
+            $post_objects = $section['section_staff'];
 
-                        <div class="nht-card__top">
+            if( $post_objects ): ?>
+                    <?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
+                        <?php setup_postdata($post); ?>
+                    <div class="nht-card nht-card--story">
+                        <div class="nht-card__inner">
 
-                            <!-- post thumbnail -->
-                            <?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                                    <?php the_post_thumbnail(array(180,180), ['class' => 'nht-card__img', 'title' => 'Feature image']); // Declare pixel size you need inside the array ?>
-                                </a>
-                            <?php endif; ?>
-                            <!-- /post thumbnail -->
+                            <div class="nht-card__top">
+
+                                <!-- post thumbnail -->
+                                <?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+                                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                        <?php the_post_thumbnail(array(180,180), ['class' => 'nht-card__img', 'title' => 'Feature image']); // Declare pixel size you need inside the array ?>
+                                    </a>
+                                <?php endif; ?>
+                                <!-- /post thumbnail -->
+
+                            </div>
+
+                            <div class="nht-card__bottom">
+
+                                <h6>
+                                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                                </h6>
+
+                                <p class="nht-card__excerpt"><?php the_excerpt(); ?></p>
+
+                                <?php get_template_part('loop-producers'); ?>
+
+                            </div>
 
                         </div>
-
-                        <div class="nht-card__bottom">
-
-                            <h6>
-                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-                            </h6>
-
-                            <p class="nht-card__excerpt"><?php the_excerpt(); ?></p>
-
-                            <?php get_template_part('loop-producers'); ?>
-
-                        </div>
-
                     </div>
-                </div>
 
-            <?php endforeach; ?>
-            <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+                <?php endforeach; ?>
+                <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+            <?php endif; ?>
+
+            <?php endwhile; ?>
         <?php endif; ?>
 
         <?php endif; ?>
