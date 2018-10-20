78f03f6f96ff59f8a1db44b89922078a4d9145e3
Henry Holtgeerts <henryholtgeerts@gmail.com>
2018-10-19 21:24:30 -0500

    OG image support for main header

--- nht-theme/header.php
+++ nht-theme/header.php
@@ -5,11 +5,10 @@
 
 		<!-- Probide basic meta info -->
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-
+		<meta property="og:title" content="<?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?>" />
+		<meta property="og:type" content="article" />
+		<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
+		<meta property="og:image" content="<?php echo ''.esc_url($featured_img_url).''; ?>" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
