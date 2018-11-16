b7f16e67c5b1499398a1ea48809ab5ad000beec2
Henry Holtgeerts <henryholtgeerts@gmail.com>
2018-11-16 10:35:32 -0600

    Reset iframe height

--- nht-theme/footer-issue.php
+++ nht-theme/footer-issue.php
@@ -5,40 +5,6 @@
         <!-- Running wp_footer func to pull in custom javascript to load in footer -->
 		<?php wp_footer(); ?>
 
-		<script type='text/javascript'>
-    
-			$(function(){
-			
-				var iFrames = $('iframe');
-			
-				function iResize() {
-				
-					for (var i = 0, j = iFrames.length; i < j; i++) {
-					iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';}
-					}
-					
-					if ($.browser.safari || $.browser.opera) { 
-					
-					iFrames.load(function(){
-						setTimeout(iResize, 0);
-					});
-					
-					for (var i = 0, j = iFrames.length; i < j; i++) {
-							var iSource = iFrames[i].src;
-							iFrames[i].src = '';
-							iFrames[i].src = iSource;
-					}
-					
-					} else {
-					iFrames.load(function() { 
-						this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
-					});
-					}
-				
-				});
-
-		</script>
-
 		<!-- analytics -->
 		<script>
 		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
